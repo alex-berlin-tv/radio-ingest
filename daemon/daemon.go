fd5bd6626153ca7a781a76758a5c7028607f8408
alex-berlin-tv <hostettler@alex-berlin.de>
2022-12-02 17:55:17 +0100

    try to parse the notification response

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -6,10 +6,12 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/alex-berlin-tv/nexx_omnia_go/notification"
 	"github.com/alex-berlin-tv/nexx_omnia_go/omnia"
 	"github.com/alex-berlin-tv/radio-ingest/config"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/sirupsen/logrus"
 )
 
 // Listens to the Omnia's notification gateway and handles incoming radio
@@ -27,15 +29,20 @@ func NewDaemon(cfg config.Config) Daemon {
 }
 
 func (d Daemon) Run() {
-
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
-	router.Post("/", func(w http.ResponseWriter, r *http.Request) {
-		dt, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(string(dt))
-	})
+	router.Post("/", d.handleNotification)
 	http.ListenAndServe(fmt.Sprintf(":%d", d.Port), router)
+}
+
+func (d Daemon) handleNotification(w http.ResponseWriter, r *http.Request) {
+	dt, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err := notification.NotificationFromJson(dt)
+	if err != nil {
+		logrus.Error(err)
+	}
+	fmt.Printf("%+v\n", data)
 }
